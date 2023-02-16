315b70fdac7d89d5e35c72df2ef6bd3ebdb299e3
Andentu <severalmaz97@gmail.com>
2023-02-16 21:09:44 +0300

    Добавил ввод имени, фамилии, логина, длины логина.

--- New_4_5_final/Program.cs
+++ New_4_5_final/Program.cs
@@ -6,8 +6,14 @@ namespace New_4_5_final
     {
         static void Main(string[] args)
         {
-            (string Name, string Surname, string Login, string LoginLength, bool HavePets, double PersonAge, string[] favcolors) User;
-
+            (string Name, string Surname, string Login, int LoginLength, bool HavePets, double PersonAge, string[] favcolors) User;
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
+            Console.WriteLine("Введите фамилию");
+            User.Surname = Console.ReadLine();
+            Console.WriteLine("Введите логин");
+            User.Login = Console.ReadLine();
+            User.LoginLength = User.Login.Length;
         }
     }
 }
