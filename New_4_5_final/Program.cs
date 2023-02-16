5192387808593ff1e402296f68f8b7164b7021dc
Andentu <severalmaz97@gmail.com>
2023-02-16 21:23:03 +0300

    Добавил цикл for для повторения ввода для каждого из трех пользователей

--- New_4_5_final/Program.cs
+++ New_4_5_final/Program.cs
@@ -6,31 +6,34 @@ namespace New_4_5_final
     {
         static void Main(string[] args)
         {
-            (string Name, string Surname, string Login, int LoginLength, bool HavePets, double PersonAge, string[] favcolors) User;
-            Console.WriteLine("Введите имя");
-            User.Name = Console.ReadLine();
-            Console.WriteLine("Введите фамилию");
-            User.Surname = Console.ReadLine();
-            Console.WriteLine("Введите логин");
-            User.Login = Console.ReadLine();
-            User.LoginLength = User.Login.Length;
-
-            Console.WriteLine("Есть ли у вас животные? Да или Нет");
-            if (Console.ReadLine() == "Да")
+            for (int j = 0; j < 3; j++)
             {
-                User.HavePets = true;
+                (string Name, string Surname, string Login, int LoginLength, bool HavePets, double PersonAge, string[] favcolors) User;
+                Console.WriteLine("Введите имя");
+                User.Name = Console.ReadLine();
+                Console.WriteLine("Введите фамилию");
+                User.Surname = Console.ReadLine();
+                Console.WriteLine("Введите логин");
+                User.Login = Console.ReadLine();
+                User.LoginLength = User.Login.Length;
+
+                Console.WriteLine("Есть ли у вас животные? Да или Нет");
+                if (Console.ReadLine() == "Да")
+                {
+                    User.HavePets = true;
+                }
+
+                Console.WriteLine("Введите ваш возраст:");
+                User.PersonAge = double.Parse(Console.ReadLine());
+                User.favcolors = new string[3];
+                Console.WriteLine("Введите три любимых цвета пользователя");
+                for (int i = 0; i < User.favcolors.Length; i++)
+                {
+                    User.favcolors[i] = Console.ReadLine();
+                }
+
+
             }
-
-            Console.WriteLine("Введите ваш возраст:");
-            User.PersonAge = double.Parse(Console.ReadLine());
-            User.favcolors = new string[3];
-            Console.WriteLine("Введите три любимых цвета пользователя");
-            for (int i = 0; i < User.favcolors.Length; i++)
-            {
-                User.favcolors[i] = Console.ReadLine();
-            }
-
-
         }
     }
 }
