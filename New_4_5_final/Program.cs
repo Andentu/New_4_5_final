82a9c7afabd15c6277403679e78048074a22b090
Andentu <severalmaz97@gmail.com>
2023-02-16 21:06:16 +0300

    Задан начальный кортедж User

--- New_4_5_final/Program.cs
+++ New_4_5_final/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace New_4_5_final
+﻿using System.Reflection.Metadata;
+
+namespace New_4_5_final
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            (string Name, string Surname, string Login, string LoginLength, bool HavePets, double PersonAge, string[] favcolors) User;
+
         }
     }
 }
